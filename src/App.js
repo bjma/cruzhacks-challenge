a77ae1b5d012418bbe2857013ed38b5e4096b0f8
Brian Ma <jingjerma@gmail.com>
2020-05-01 02:05:52 -0700

    started on announcements, not fetching for some reason

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Form from './components/Form'
+import Announcements from './components/Announcements'
 import './App.css';
 
 class App extends React.Component {
     render() {
         return (
             <div className='App'>
+                <Announcements />
                 <Form />
             </div>
         )
--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import moduleName from 'material-ui/styles/MuiThemeProvider' 
+
+class Announcements extends React.Component {
+    async componentDidMount() {
+        const url = 'https://us-central1-cruzhacks-test-challenge-65ccc.cloudfunctions.net/getDB'
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    render() {
+        return <h1>HI!</h1>
+    }
+}
+
+export default Announcements
